dd37a1721631536f0e3640fdaddbef8ad9f1c101
aliszhuravl <alis.zhuravl@gmail.com>
2024-05-23 13:17:54 +0300

    initial

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+//= parts/_viewport.js
+//= parts/_ie.js
+//= parts/_link-highlighting.js
+//= parts/_scrolling.js
+//= parts/_dd.js
+//= parts/_tabs.js
+//= parts/_menu-ds.js
+//= parts/_accordion.js
+//= parts/_hamburger.js
+//= parts/_fancybox.js
