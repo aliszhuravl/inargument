603aeaacb22df40ae7289834bff99dec8d102320
aliszhuravl <alis.zhuravl@gmail.com>
2024-10-22 18:40:21 +0300

    mini_menu
    Добавила мини-меню к делам

--- src/js/parts/_things.js
+++ src/js/parts/_things.js
@@ -4,3 +4,17 @@
         $('.btn_show').addClass('hidden');
     });
 })(jQuery);
+
+(function($) {
+    const handleClick = event => {
+        const $target = $(event.target).next();
+        $target.toggleClass("hidden");
+        $(".more_menu").each(function() {
+            if ($(this)[0] !== $target[0]) {
+                $(this).addClass("hidden")
+            }
+        })
+    };
+
+    $(".more_btn").on("click", handleClick);
+})(jQuery);
--- web/js/main.js
+++ web/js/main.js
@@ -270,6 +270,20 @@ function onYouTubePlayerAPIReady() {
         $('.btn_show').addClass('hidden');
     });
 })(jQuery);
+
+(function($) {
+    const handleClick = event => {
+        const $target = $(event.target).next();
+        $target.toggleClass("hidden");
+        $(".more_menu").each(function() {
+            if ($(this)[0] !== $target[0]) {
+                $(this).addClass("hidden")
+            }
+        })
+    };
+
+    $(".more_btn").on("click", handleClick);
+})(jQuery);
 (function($) {
 
     $('.sticky').hcSticky({
